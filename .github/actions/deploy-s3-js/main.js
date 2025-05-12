4b2ce0901fe9c936d3dfc4646d7d39399cb5a02b
YassineIY <yassine.ibnyaich@gmail.com>
2025-05-12 21:46:48 +0200

    added new js upload to bucket

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,7 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
+const exec = require('@actions/exec')
 function run() {
+
+  const bucket = core.getInput('bucket', { required: true})
+  const bucketRegion = core.getInput('bucker-region', {required: true})
+  const distFolder = core.getInput('dist-folder', {required: true})
+
+  // 2) upload the files
+  const s3Uri = `s3://${bucket}`
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri}`)
+
   core.notice('Hello from my custom JavaScript Action! changes')
 }
 
