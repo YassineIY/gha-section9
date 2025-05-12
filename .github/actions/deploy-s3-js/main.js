ffb0536ee74e5af946a0ebe1014be0d3bd4a5258
YassineIY <yassine.ibnyaich@gmail.com>
2025-05-12 21:50:32 +0200

    fixed some issues

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -4,12 +4,13 @@ const exec = require('@actions/exec')
 function run() {
 
   const bucket = core.getInput('bucket', { required: true})
-  const bucketRegion = core.getInput('bucker-region', {required: true})
+  const bucketRegion = core.getInput('bucket-region', {required: true})
   const distFolder = core.getInput('dist-folder', {required: true})
 
   // 2) upload the files
-  const s3Uri = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri}`)
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
 
   core.notice('Hello from my custom JavaScript Action! changes')
 }
